4ac97602d3797fc83e79df03fa0d7f06132e0fbd
chernataly2020 <cherkesnataly@gmail.com>
2021-04-13 00:14:42 +0300

    feat: solve task 10-find-index.js

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -12,7 +12,24 @@
  *
  */
 function findIndex(array, value) {
-  return +(array.findIndex(value));
+  let first = 0; // left endpoint
+  let last = array.length - 1; // right endpoint
+  let position = -1;
+  let found = false;
+  let middle;
+
+  while (found === false && first <= last) {
+    middle = Math.floor((first + last) / 2);
+    if (array[middle] === value) {
+      found = true;
+      position = middle;
+    } else if (array[middle] > value) { // if in lower half
+      last = middle - 1;
+    } else { // in in upper half
+      first = middle + 1;
+    }
+  }
+  return position;
 }
 
 module.exports = findIndex;
